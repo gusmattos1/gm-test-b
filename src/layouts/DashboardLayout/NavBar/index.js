17304291662c49b91f13911bc05b954464e57c01
Gustavo <gusmattos1@gmail.com>
2021-03-14 18:49:58 -0300

    Includes default layouts

---
+++ src/layouts/DashboardLayout/NavBar/index.js
@@ -0,0 +1,135 @@
+import React, { useEffect } from "react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Drawer,
+  Hidden,
+  List,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import PersonIcon from "@material-ui/icons/Person";
+
+import NavItem from "./NavItem";
+
+const items = [
+  {
+    href: "/dashboard",
+    icon: DashboardIcon,
+    title: "Dashboard",
+  },
+  {
+    href: "/newdashboard",
+    icon: StarBorderIcon,
+    title: "New Dashboard",
+  },
+  {
+    href: "/profile",
+    icon: PersonIcon,
+    title: "Profile",
+  },
+];
+
+const useStyles = makeStyles(() => ({
+  mobileDrawer: {
+    width: 256,
+  },
+  desktopDrawer: {
+    width: 256,
+    top: 64,
+    height: "calc(100% - 64px)",
+  },
+  avatar: {
+    cursor: "pointer",
+    width: 64,
+    height: 64,
+  },
+}));
+
+const NavBar = ({ onMobileClose, openMobile, user }) => {
+  const classes = useStyles();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (openMobile && onMobileClose) {
+      onMobileClose();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.pathname]);
+
+  const content = (
+    <Box height="100%" display="flex" flexDirection="column">
+      <Box alignItems="center" display="flex" flexDirection="column" p={2}>
+        <Avatar
+          className={classes.avatar}
+          component={RouterLink}
+          src={user?.imageUrl}
+          to="/profile"
+        />
+        <Typography className={classes.name} color="textPrimary" variant="h5">
+          {user?.name || ""}
+        </Typography>
+        <Typography color="textSecondary" variant="body2">
+          {user?.email || ""}
+        </Typography>
+      </Box>
+      <Divider />
+      <Box p={2}>
+        <List>
+          {items.map((item) => (
+            <NavItem
+              href={item.href}
+              key={item.title}
+              title={item.title}
+              icon={item.icon}
+            />
+          ))}
+        </List>
+      </Box>
+      <Box flexGrow={1} />
+    </Box>
+  );
+
+  return (
+    <>
+      <Hidden lgUp>
+        <Drawer
+          anchor="left"
+          classes={{ paper: classes.mobileDrawer }}
+          onClose={onMobileClose}
+          open={openMobile}
+          variant="temporary"
+        >
+          {content}
+        </Drawer>
+      </Hidden>
+      <Hidden mdDown>
+        <Drawer
+          anchor="left"
+          classes={{ paper: classes.desktopDrawer }}
+          open
+          variant="persistent"
+        >
+          {content}
+        </Drawer>
+      </Hidden>
+    </>
+  );
+};
+
+NavBar.propTypes = {
+  onMobileClose: PropTypes.func,
+  openMobile: PropTypes.bool,
+};
+
+NavBar.defaultProps = {
+  onMobileClose: () => {},
+  openMobile: false,
+};
+
+export default NavBar;
---
+++ src/layouts/DashboardLayout/TopBar.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import {
+  AppBar,
+  Badge,
+  Box,
+  Hidden,
+  IconButton,
+  Toolbar,
+  makeStyles,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import NotificationsIcon from "@material-ui/icons/NotificationsOutlined";
+import Logo from "src/assets/icons/logo.png";
+import Logout from "src/views/Logout/Logout";
+
+const useStyles = makeStyles(() => ({
+  root: {},
+  avatar: {
+    width: 60,
+    height: 60,
+  },
+  logo: {
+    height: 60,
+  },
+}));
+
+const TopBar = ({ className, onMobileNavOpen, setUserLogged, ...rest }) => {
+  const classes = useStyles();
+  const [notifications] = useState([]);
+
+  return (
+    <AppBar className={clsx(classes.root, className)} elevation={0} {...rest}>
+      <Toolbar>
+        <RouterLink to="/">
+          <img src={Logo} alt="Logo" className={classes.logo} />
+        </RouterLink>
+        <Box flexGrow={1} />
+        <Hidden mdDown>
+          <IconButton color="inherit">
+            <Badge
+              badgeContent={notifications.length}
+              color="primary"
+              variant="dot"
+            >
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton color="inherit">
+            <Logout setUserLogged={setUserLogged} />
+          </IconButton>
+        </Hidden>
+        <Hidden lgUp>
+          <IconButton color="inherit" onClick={onMobileNavOpen}>
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+TopBar.propTypes = {
+  className: PropTypes.string,
+  onMobileNavOpen: PropTypes.func,
+};
+
+export default TopBar;
